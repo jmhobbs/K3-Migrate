bfce0c9f06aa3753906792c75991f266ea329182
John Hobbs <john@velvetcache.org>
2011-02-25 17:38:19 -0600

    Encapsulated the migration work into the Migrations class.

--- Migration/Migration.php
+++ Migration/Migration.php
@@ -1,6 +1,10 @@
 <?php
 
-	class Migration {
+	abstract class Migration {
+
+		public abstract function up ();
+		public abstract function down ();
+
 		public static function CreateTable ( $name, $args = null ) {
 			return new Migration_Statement_CreateTable( $name, $args );
 		}
--- example.php
+++ example.php
@@ -2,6 +2,18 @@
 
 	require_once( 'Migration/include.php' );
 
-	$table = Migration::CreateTable( 'User', array( 'modified' => false ) );
-	$table->addColumn( 'string', 'name', array( 'default' => 'John' ) );
-	die( $table->toSQL() );
+	class UserMigration extends Migration {
+		public function up () {
+			$table = self::CreateTable( 'User', array( 'modified' => false ) );
+			$table->addColumn( 'string', 'name', array( 'default' => 'John' ) );
+			return $table->toSQL();
+		}
+
+		public function down () {
+			return "";
+		}
+	}
+
+	//////// Runner Code ////////
+	$migration = new UserMigration();
+	die( $migration->up() );
