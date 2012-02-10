feb59f457d9b9b0f3fea34b8fb76bbf57036e492
Alexandr Prudnikov <al.prudnikov@gmail.com>
2012-02-11 00:24:38 +0300

    PHPDoc for Kohana_Migration_Statement

--- classes/kohana/migration/statement.php
+++ classes/kohana/migration/statement.php
@@ -5,29 +5,26 @@
 		protected $_before = array();
 		protected $_after = array();
 
-		/*!
-			Return the SQL required to execute this statement.
-
-			\returns String
+		/**
+		 * Return the SQL required to execute this statement.
+		 * @return string
 		*/
 		abstract public function toSQL ();
 
-		/*!
-			Register a hook to run before the statement is executed.
-
-			Only ran on top level statements (CreatTable, ChangeTable, DropTable)
-		*/
+		/**
+		 * Register a hook to run before the statement is executed.
+		 * Only ran on top level statements (CreateTable, ChangeTable, DropTable)
+		 * @param Closure|array $fn
+		 */
 		public function runBefore ( $fn ) {
 			$this->_before[] = $fn;
 		}
 
-		/*!
-			Execute hooks registered for execution before query.
-
-			\param migration The Kohana_Migration running this statement.
-			\param database The Database object the Kohana_Migration is using.
-
-			\returns Boolean, true if all hooks return true, false if any return false.
+		/**
+		 * Execute hooks registered for execution before query.
+		 * @param string migration - The Kohana_Migration running this statement.
+		 * @param Database database - The Database object the Kohana_Migration is using.
+		 * @return bool, true if all hooks return true, false if any return false.
 		*/
 		public function before ( $migration, $database ) {
 			foreach( $this->_before as $before ) {
@@ -38,22 +35,20 @@
 			return true;
 		}
 
-		/*!
-			Register a hook to run after the statement is executed.
-
-			Only ran on top level statements (CreatTable, ChangeTable, DropTable)
+		/**
+		 * Register a hook to run after the statement is executed.
+		 * Only ran on top level statements (CreateTable, ChangeTable, DropTable)
+		 * @param Closure|array $fn
 		*/
 		public function runAfter ( $fn ) {
 			$this->_after[] = $fn;
 		}
 
-		/*!
-			Execute hooks registered for execution after query.
-
-			\param migration The Kohana_Migration running this statement.
-			\param database The Database object the Kohana_Migration is using.
-
-			\returns Boolean, true if all hooks return true, false if any return false.
+		/**
+		 * Execute hooks registered for execution after query.
+		 * @param string migration - The Kohana_Migration running this statement.
+		 * @param Database database - The Database object the Kohana_Migration is using.
+		 * @return bool, true if all hooks return true, false if any return false.
 		*/
 		public function after ( $migration, $database ) {
 			foreach( $this->_after as $after ) {
